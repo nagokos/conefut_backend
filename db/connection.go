12c471e5710b897a65c2ad9c39d3c37daa3d2546
naohiro <your.email@kosuda0428@icloud.com>
2022-01-09 19:53:04 +0900

    add connection.go

---
+++ db/connection.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/lib/pq"
+	"github.com/nagokos/connefut_backend/ent"
+	"github.com/nagokos/connefut_backend/logger"
+	sqldblogger "github.com/simukti/sqldb-logger"
+)
+
+func DatabaseConnection() *ent.Client {
+	client, err := sql.Open("postgres", "host=db dbname=connefut_db port=5432 user=root password=password sslmode=disable")
+
+	if err != nil {
+		logger.Log.Fatal().Msg(fmt.Sprintf("failed opening connection to postgres: %v", err))
+	}
+
+	db := sqldblogger.OpenDriver(
+		"postgres://root:password@db:5432/connefut_db?sslmode=disable",
+		client.Driver(),
+		&logger.Logger{},
+		sqldblogger.WithWrapResult(false),
+		sqldblogger.WithSQLQueryAsMessage(true),
+		sqldblogger.WithPreparerLevel(sqldblogger.LevelDebug),
+		sqldblogger.WithQueryerLevel(sqldblogger.LevelDebug),
+		sqldblogger.WithExecerLevel(sqldblogger.LevelDebug),
+	)
+
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	return ent.NewClient(ent.Driver(drv))
+}
