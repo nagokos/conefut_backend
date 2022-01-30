38fb65dd1b3920141096b0960cdc870c8c292e9f
naohiro <your.email@kosuda0428@icloud.com>
2022-01-30 22:11:53 +0900

    add recruitment.go

---
+++ ent/schema/recruitment.go
@@ -0,0 +1,61 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/field"
+)
+
+// Recruitment holds the schema definition for the Recruitment entity.
+type Recruitment struct {
+	ent.Schema
+}
+
+func (Recruitment) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		UUIDMixin{},
+		TimeMixin{},
+	}
+}
+
+// Fields of the Recruitment.
+func (Recruitment) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("title").
+			MaxLen(60).
+			SchemaType(map[string]string{
+				dialect.Postgres: "varchar(60)",
+			}),
+		field.Enum("type").
+			Values(
+				"opponent",
+				"individual",
+				"teammate",
+				"joining",
+				"coaching",
+				"others",
+			).
+			Default("opponent"),
+		field.String("place").
+			Optional(),
+		field.Time("start_at").
+			Optional(),
+		field.String("content").
+			MaxLen(10000).
+			SchemaType(map[string]string{
+				dialect.Postgres: "varchar(10000)",
+			}).
+			Comment("募集詳細"),
+		field.String("Location_url").
+			Optional().
+			Comment("会場の場所を埋め込むURL"),
+		field.Int("capacity"),
+		field.Time("closing_at").
+			Comment("募集期限"),
+	}
+}
+
+// Edges of the Recruitment.
+func (Recruitment) Edges() []ent.Edge {
+	return nil
+}
