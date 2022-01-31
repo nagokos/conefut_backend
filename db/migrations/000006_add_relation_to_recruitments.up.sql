939f0d83b74e54232de164f08358284ca7a74cab
naohiro <your.email@kosuda0428@icloud.com>
2022-02-01 05:54:28 +0900

    コードを修正

--- db/migrations/000006_add_relation_to_recruitments.up.sql
+++ db/migrations/000006_add_relation_to_recruitments.up.sql
@@ -1,1 +1,13 @@
-ALTER TABLE "recruitments" ADD CONSTRAINT "recruitments_users_recruitments" FOREIGN KEY("user_id") REFERENCES "users"("id") ON DELETE CASCADE;
+ALTER TABLE "recruitments"
+  ADD CONSTRAINT "recruitments_competitions_recruitments"
+    FOREIGN KEY("competition_id")
+    REFERENCES "competitions"("id")
+    ON DELETE RESTRICT,
+  ADD CONSTRAINT "recruitments_prefectures_recruitments"
+    FOREIGN KEY("prefecture_id")
+    REFERENCES "prefectures"("id")
+    ON DELETE RESTRICT,
+  ADD CONSTRAINT "recruitments_users_recruitments"
+    FOREIGN KEY("user_id")
+    REFERENCES "users"("id")
+    ON DELETE CASCADE;
