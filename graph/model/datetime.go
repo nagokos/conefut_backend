140d2e652540c732d93b03a625b2a9521928cacf
naohiro <your.email@kosuda0428@icloud.com>
2022-02-07 00:31:35 +0900

    カスタムスカラを作成

---
+++ graph/model/datetime.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+/*
+	APIサーバで受け取ったdatetimeフィールドの値をtime.Time型に変換する。
+*/
+func UnmarshalDateTime(v interface{}) (time.Time, error) {
+	switch v := v.(type) {
+	case string:
+		return time.Parse("2006/01/02 15:04", v)
+	case time.Time:
+		return v, nil
+	default:
+		return time.Now(), fmt.Errorf("DateTime is invalid")
+	}
+}
+
+/*
+	APIサーバからJSONを返す際に、time.Time型をstringに変換する。
+*/
+func MarshalDateTime(t time.Time) graphql.Marshaler {
+	return graphql.WriterFunc(func(w io.Writer) {
+		w.Write([]byte(strconv.Quote(t.Format("2006/01/02 15:04"))))
+	})
+}
