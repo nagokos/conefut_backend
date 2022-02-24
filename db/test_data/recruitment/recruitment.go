05a75654960cec302972a80e9a99cdc10d188505
nagokos <kosuda0428@icloud.com>
2022-02-24 23:13:41 +0900

    add recruitment.go

---
+++ db/test_data/recruitment/recruitment.go
@@ -0,0 +1,81 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/nagokos/connefut_backend/db"
+	"github.com/nagokos/connefut_backend/ent"
+	"github.com/nagokos/connefut_backend/logger"
+)
+
+func main() {
+	client := db.DatabaseConnection()
+	defer client.Close()
+
+	ctx := context.Background()
+
+	comp, err := client.Competition.Query().First(ctx)
+	if err != nil {
+		logger.Log.Error().Msg(err.Error())
+		return
+	}
+
+	pref, err := client.Prefecture.Query().First(ctx)
+	if err != nil {
+		logger.Log.Error().Msg(err.Error())
+		return
+	}
+
+	user, err := client.User.Query().First(ctx)
+	if err != nil {
+		logger.Log.Error().Msg(err.Error())
+		return
+	}
+
+	var recruitments []*ent.Recruitment
+	for i := 0; i < 20; i++ {
+		recruitment := &ent.Recruitment{
+			Type:      "opponent",
+			Level:     "enjoy",
+			Title:     fmt.Sprintf("%v 明日の午後からサッカーできる人を探しています。ご連絡お待ちしています。", i),
+			Content:   fmt.Sprintf("%v 明日の午後からサッカーできる人を探しています。場所は後ほど連絡します。", i),
+			Place:     "埼玉スタジアム2002",
+			StartAt:   time.Now().Add(time.Hour * 5),
+			ClosingAt: time.Now().Add(time.Hour * 2),
+			Edges: ent.RecruitmentEdges{
+				Competition: comp,
+				Prefecture:  pref,
+				User:        user,
+			},
+		}
+		recruitments = append(recruitments, recruitment)
+	}
+
+	bulk := make([]*ent.RecruitmentCreate, len(recruitments))
+
+	for i, rec := range recruitments {
+		bulk[i] = client.Recruitment.
+			Create().
+			SetTitle(rec.Title).
+			SetType(rec.Type).
+			SetLevel(rec.Level).
+			SetNillableStartAt(&rec.StartAt).
+			SetNillableContent(&rec.Content).
+			SetNillablePlace(&rec.Place).
+			SetIsPublished(true).
+			SetNillableClosingAt(&rec.ClosingAt).
+			SetNillableCompetitionID(&rec.Edges.Competition.ID).
+			SetNillablePrefectureID(&rec.Edges.Prefecture.ID).
+			SetUserID(rec.Edges.User.ID)
+	}
+
+	_, err = client.Recruitment.CreateBulk(bulk...).Save(ctx)
+	if err != nil {
+		logger.Log.Error().Msg(err.Error())
+	}
+}
