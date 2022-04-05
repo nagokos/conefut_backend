179a458fb981e480cdb0ccaf9fd29f8e0d233c4b
Naohiro Kosuda <kosnago0428@gmail.com>
2022-04-06 00:12:34 +0900

    entからdatabase/sqlに修正

--- graph/models/applicant/applicant.go
+++ graph/models/applicant/applicant.go
@@ -10,70 +10,55 @@ import (
 	"github.com/nagokos/connefut_backend/logger"
 )
 
-func CheckApplied(ctx context.Context, client ent.Client, recId string) (bool, error) {
+func CheckApplied(ctx context.Context, dbConnection *sql.DB, recID string) (bool, error) {
 	currentUser := auth.ForContext(ctx)
 
-	isApplied, err := client.Applicant.
-		Query().
-		Where(
-			applicant.UserID(currentUser.ID),
-			applicant.RecruitmentID(recId),
-		).
-		Exist(ctx)
+	cmd := `
+	  SELECT COUNT(DISTINCT a.id) 
+		FROM applicants AS a
+		WHERE a.user_id = $1
+		AND a.recruitment_id = $2
+	`
+
+	row := dbConnection.QueryRow(cmd, currentUser.ID, recID)
+
+	var count int
+	err := row.Scan(&count)
 	if err != nil {
 		logger.NewLogger().Error(err.Error())
 		return false, err
+	}
+
+	var isApplied bool
+	if count == 1 {
+		isApplied = true
 	}
 
 	return isApplied, nil
 }
 
-func GetAppliedCounts(ctx context.Context, client ent.Client, recId string) (int, error) {
-	res, err := client.Applicant.
-		Query().
-		Where(
-			applicant.RecruitmentID(recId),
-		).
-		Count(ctx)
+func GetAppliedCounts(dbConnection *sql.DB, recID string) (int, error) {
+	cmd := `
+	  SELECT COUNT(DISTINCT a.id)
+		FROM applicants AS a
+		WHERE a.recruitment_id = $1
+	`
+	row := dbConnection.QueryRow(cmd, recID)
+
+	var count int
+	err := row.Scan(&count)
 	if err != nil {
 		logger.NewLogger().Error(err.Error())
-		return res, err
+		return count, err
 	}
 
-	return res, nil
+	return count, nil
 }
 
-func CreateApplicant(ctx context.Context, client ent.Client, recId string, status model.ManagementStatus) (bool, error) {
+func CreateApplicant(ctx context.Context, dbConnection *sql.DB, recId string, status model.ManagementStatus) (bool, error) {
 	currentUser := auth.ForContext(ctx)
 	if currentUser == nil {
 		return false, errors.New("ログインしてください")
-	}
-
-	isApplied, err := client.Applicant.
-		Query().
-		Where(
-			applicant.UserID(currentUser.ID),
-			applicant.RecruitmentID(recId),
-		).
-		Exist(ctx)
-	if err != nil {
-		logger.NewLogger().Error(err.Error())
-	}
-
-	if isApplied {
-		logger.NewLogger().Error("already applied for this position")
-		return false, errors.New("この募集には既に応募しています")
-	}
-
-	_, err = client.Applicant.
-		Create().
-		SetRecruitmentID(recId).
-		SetManagementStatus(applicant.ManagementStatus(strings.ToLower(string(status)))).
-		SetUserID(currentUser.ID).
-		Save(ctx)
-	if err != nil {
-		logger.NewLogger().Error(err.Error())
-		return false, err
 	}
 
 	return true, nil
