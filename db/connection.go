914ac73f80c2b843a1806579e70182579a6939ce
Naohiro Kosuda <kosnago0428@gmail.com>
2022-04-08 04:16:14 +0900

    queryログをpgxのものを使用

--- db/connection.go
+++ db/connection.go
@@ -17,21 +17,12 @@ func DatabaseConnection() *pgxpool.Pool {
 		logger.NewLogger().Sugar().Fatalf("failed opening connection to postgres: %v", err)
 	}
 
-	err = client.Ping()
+	cfg.ConnConfig.Logger = zapadapter.NewLogger(logger.NewLogger())
+	cfg.ConnConfig.LogLevel = pgx.LogLevelDebug
+	dbPool, err := pgxpool.ConnectConfig(context.Background(), cfg)
 	if err != nil {
-		logger.NewLogger().Sugar().Fatalf("pingError: %v", err)
+		panic(err)
 	}
 
-	db := sqldblogger.OpenDriver(
-		"postgres://root:password@db:5432/connefut_db?sslmode=disable",
-		client.Driver(),
-		&logger.Logger{},
-		sqldblogger.WithWrapResult(false),
-		sqldblogger.WithSQLQueryAsMessage(true),
-		sqldblogger.WithPreparerLevel(sqldblogger.LevelDebug),
-		sqldblogger.WithQueryerLevel(sqldblogger.LevelDebug),
-		sqldblogger.WithExecerLevel(sqldblogger.LevelDebug),
-	)
-
-	return db
+	return dbPool
 }
