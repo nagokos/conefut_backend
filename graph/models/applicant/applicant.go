e4117e677d4cb82d80c4626498aed8995b005173
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-04 05:27:41 +0900

    ロジックの修正

--- graph/models/applicant/applicant.go
+++ graph/models/applicant/applicant.go
@@ -3,31 +3,28 @@ package applicant
 import (
 	"context"
 	"errors"
-	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/nagokos/connefut_backend/auth"
-	"github.com/nagokos/connefut_backend/graph/model"
 	"github.com/nagokos/connefut_backend/logger"
 	"github.com/rs/xid"
 )
 
-func CheckApplied(ctx context.Context, dbPool *pgxpool.Pool, recID string) (bool, error) {
+func CheckAppliedForRecruitment(ctx context.Context, dbPool *pgxpool.Pool, recID string) (bool, error) {
 	currentUser := auth.ForContext(ctx)
-
 	if currentUser == nil {
 		return false, nil
 	}
 
 	cmd := `
-	  SELECT COUNT(DISTINCT a.id) 
-		FROM applicants AS a
-		WHERE a.user_id = $1
-		AND a.recruitment_id = $2
+	  SELECT COUNT(DISTINCT a.id)
+		FROM applicants AS a 
+		WHERE a.recruitment_id = $1
+		AND a.user_id = $2
 	`
 
-	row := dbPool.QueryRow(ctx, cmd, currentUser.ID, recID)
+	row := dbPool.QueryRow(ctx, cmd, recID, currentUser.ID)
 
 	var count int
 	err := row.Scan(&count)
@@ -37,7 +34,7 @@ func CheckApplied(ctx context.Context, dbPool *pgxpool.Pool, recID string) (bool
 	}
 
 	var isApplied bool
-	if count == 1 {
+	if count != 0 {
 		isApplied = true
 	}
 
@@ -62,7 +59,7 @@ func GetAppliedCounts(ctx context.Context, dbPool *pgxpool.Pool, recID string) (
 	return count, nil
 }
 
-func CreateApplicant(ctx context.Context, dbPool *pgxpool.Pool, recId string, status model.ManagementStatus) (bool, error) {
+func CreateApplicant(ctx context.Context, dbPool *pgxpool.Pool, recId string) (bool, error) {
 	currentUser := auth.ForContext(ctx)
 	if currentUser == nil {
 		return false, errors.New("ログインしてください")
@@ -85,16 +82,16 @@ func CreateApplicant(ctx context.Context, dbPool *pgxpool.Pool, recId string, st
 
 	cmd = `
 	  INSERT INTO applicants 
-		  (id, management_status, recruitment_id, user_id, created_at, updated_at)
+		  (id, recruitment_id, user_id, created_at, updated_at)
 		VALUES 
-		  ($1, $2, $3, $4, $5, $6)
+		  ($1, $2, $3, $4, $5)
 	`
 
 	timeNow := time.Now().Local()
 
 	_, err = dbPool.Exec(
 		ctx, cmd,
-		xid.New().String(), strings.ToLower(string(status)), recId, currentUser.ID, timeNow, timeNow,
+		xid.New().String(), recId, currentUser.ID, timeNow, timeNow,
 	)
 
 	if err != nil {
