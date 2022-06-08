afef63a50b00907ff28a5c95be1e3b9c3434e8f6
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-09 06:45:15 +0900

    columを修正

--- db/migrations/000030_create_messages_table.up.sql
+++ db/migrations/000030_create_messages_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "messages"(
   "id" VARCHAR NOT NULL,
   "content" VARCHAR(1000) NOT NULL,
   "room_id" VARCHAR NOT NULL,
-  "user_id" VARCHAR NOT NULL,
+  "user_id" VARCHAR NULL,
   PRIMARY KEY("id"),
   FOREIGN KEY("room_id") 
     REFERENCES "rooms"("id")
