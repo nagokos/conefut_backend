d346a7a322c8a4dab924bf887565b14bdac9c61b
Naohiro Kosuda <kosnago0428@gmail.com>
2022-06-24 14:06:50 +0900

    ユーザーの部屋一覧を取得する関数を作成

--- graph/models/room/room.go
+++ graph/models/room/room.go
@@ -34,3 +34,55 @@ func CreateRoom(ctx context.Context, tx pgx.Tx) (string, error) {
 
 	return roomID, nil
 }
+
+func GetCurrentUserRooms(ctx context.Context, dbPool *pgxpool.Pool) ([]*model.Room, error) {
+	currenUser := auth.ForContext(ctx)
+	if currenUser == nil {
+		logger.NewLogger().Error("user not loggedIn")
+		return nil, errors.New("ログインしてください")
+	}
+
+	cmd := `
+	  SELECT e_1.room_id, u.name, u.avatar
+		FROM entries AS e_1
+		INNER JOIN (
+			SELECT room_id 
+			FROM entries 
+			WHERE user_id = $1
+		) AS e_2
+		ON e_1.room_id = e_2.room_id
+		INNER JOIN users AS u 
+		ON e_1.user_id = u.id
+		WHERE NOT e_1.user_id = $2
+	`
+
+	rows, err := dbPool.Query(
+		ctx, cmd,
+		currenUser.ID, currenUser.ID,
+	)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var rooms []*model.Room
+	for rows.Next() {
+		var room model.Room
+		var entrie model.Entrie
+		var user model.User
+		rows.Scan(&room.ID, &user.Name, &user.Avatar)
+		entrie.User = &user
+		room.Entrie = &entrie
+		rooms = append(rooms, &room)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+
+	return rooms, nil
+}
