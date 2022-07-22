795ef8952a1499bad7571ed5736e7f4c1b68314c
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-23 02:01:51 +0900

    add user.go

---
+++ graph/model/user.go
@@ -0,0 +1,14 @@
+package model
+
+func (User) IsNode() {}
+
+type User struct {
+	ID                      string                  `json:"id"`
+	DatabaseID              int                     `json:"databaseId"`
+	Name                    string                  `json:"name"`
+	Email                   string                  `json:"email"`
+	Avatar                  string                  `json:"avatar"`
+	Introduction            *string                 `json:"introduction"`
+	Role                    Role                    `json:"role"`
+	EmailVerificationStatus EmailVerificationStatus `json:"emailVerificationStatus"`
+}
