e85f79591b3c44b3a727adac4d48aa628dcc25da
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-25 18:45:44 +0900

    add func

--- graph/utils/utils.go
+++ graph/utils/utils.go
@@ -15,6 +15,16 @@ func GenerateUniqueID(tableName string, id int) string {
 	return encodeString
 }
 
+func DecodeTableName(id string) string {
+	dec, err := base64.URLEncoding.DecodeString(id)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+	}
+	split := strings.Split(string(dec), ":")
+	tableName := split[0]
+	return tableName
+}
+
 func DecodeUniqueID(id string) int {
 	dec, err := base64.URLEncoding.DecodeString(id)
 	if err != nil {
