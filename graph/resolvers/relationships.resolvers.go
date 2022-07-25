946a1610f223c4858ce3292a303e7c2cc2d52764
Naohiro Kosuda <kosnago0428@gmail.com>
2022-07-25 18:48:19 +0900

    relationships.resolverの実装　

--- graph/resolvers/relationships.resolvers.go
+++ graph/resolvers/relationships.resolvers.go
@@ -5,22 +5,48 @@ package resolvers
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/nagokos/connefut_backend/graph/model"
+	"github.com/nagokos/connefut_backend/graph/models/relationship"
+	"github.com/nagokos/connefut_backend/logger"
 )
 
 // Follow is the resolver for the follow field.
 func (r *mutationResolver) Follow(ctx context.Context, userID string) (*model.FeedbackFollow, error) {
-	panic(fmt.Errorf("not implemented"))
+	feedback, err := relationship.Follow(ctx, r.dbPool, userID)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+	return feedback, nil
 }
 
 // UnFollow is the resolver for the unFollow field.
 func (r *mutationResolver) UnFollow(ctx context.Context, userID string) (*model.FeedbackFollow, error) {
-	panic(fmt.Errorf("not implemented"))
+	feedback, err := relationship.UnFollow(ctx, r.dbPool, userID)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+	return feedback, nil
 }
 
 // CheckFollowed is the resolver for the checkFollowed field.
 func (r *queryResolver) CheckFollowed(ctx context.Context, userID string) (*model.FeedbackFollow, error) {
-	panic(fmt.Errorf("not implemented"))
+	feedback, err := relationship.CheckFollowed(ctx, r.dbPool, userID)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+	return feedback, nil
+}
+
+// CheckFollowedByRecruitmentID is the resolver for the checkFollowedByRecruitmentId field.
+func (r *queryResolver) CheckFollowedByRecruitmentID(ctx context.Context, recruitmentID string) (*model.FeedbackFollow, error) {
+	feedback, err := relationship.CheckFollowedByRecruitmentID(ctx, r.dbPool, recruitmentID)
+	if err != nil {
+		logger.NewLogger().Error(err.Error())
+		return nil, err
+	}
+	return feedback, nil
 }
