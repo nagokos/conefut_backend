010616b2e93a87b6b5a0a9d392b85ae7e0ca7bd8
Naohiro Kosuda <kosnago0428@gmail.com>
2022-08-01 13:53:45 +0900

    add field

--- graph/model/user.go
+++ graph/model/user.go
@@ -7,6 +7,7 @@ type User struct {
 	DatabaseID              int                     `json:"databaseId"`
 	Name                    string                  `json:"name"`
 	Email                   string                  `json:"email"`
+	UnverifiedEmail         *string                 `json:"unverifiedEmail"`
 	Avatar                  string                  `json:"avatar"`
 	Introduction            *string                 `json:"introduction"`
 	Role                    Role                    `json:"role"`
