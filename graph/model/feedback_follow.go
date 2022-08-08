235922efb2719cc20f3f18006cfd231c17c0123c
Naohiro Kosuda <nagokos0428@gmail.com>
2022-08-08 12:32:30 +0900

    Revert "add feedback_stock.go"
    This reverts commit 61aab81368626a3c84bb8555d3f482cd385b8786.

--- graph/model/feedback_follow.go
+++
@@ -1,9 +0,0 @@
-package model
-
-func (FeedbackFollow) IsNode() {}
-
-type FeedbackFollow struct {
-	ID               string `json:"id"`
-	IsViewerFollowed bool   `json:"isViewerFollowed"`
-	UserID           int    `json:"userId"`
-}
